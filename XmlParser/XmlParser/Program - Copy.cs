63c2b736f5a7e2663f7c96d3854785794bf25488
ewasow <sow.ewa@gmail.com>
2016-01-31 21:56:09 +0100

    Added XmlParser project

---
+++ XmlParser/XmlParser/Program - Copy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = XDocument.Load("Config.xml");   
+            var output = new XDocument(input.Declaration);
+            var outputRoot = copyWithoutChildren(input.Root);
+            output.Add(outputRoot);
+            foreach (var node in getAllLeaves(input))
+            {
+                var newNode = createOutputNode(node, input);
+                outputRoot.Add(newNode);
+            }
+            Console.WriteLine(output);
+            output.Save("output.xml");
+            Console.Read();
+        }
+
+        private static XElement createOutputNode(XElement inputNode, XDocument input)
+        {
+            var outputNode = new XElement(inputNode.Name);
+            setAttributesWithParentsNames(inputNode, input.Root, outputNode);
+            outputNode.Add(inputNode.Attributes());
+            return outputNode;
+        }
+
+        private static void setAttributesWithParentsNames(XElement inputNode, XElement inputRoot, XElement outputNode)
+        {
+            var parent = inputNode.Parent;
+            int level = 0;
+            while (parent != inputRoot)
+            {
+                var attrName = getParentRelatedAttributeName(level);
+                outputNode.SetAttributeValue(attrName, parent.Name);
+                level++;
+                parent = parent.Parent;
+            }
+        }
+
+        private static string getParentRelatedAttributeName(int level)
+        {
+            if (level == 0)
+            {
+                return "parent";
+            }
+            return string.Join("", Enumerable.Repeat("grand", level));
+        }
+
+        private static XElement copyWithoutChildren(XElement element)
+        {
+            var newElement = new XElement(element);
+            newElement.RemoveNodes();
+            return newElement;
+        }
+
+        private static IEnumerable<XElement> getAllLeaves(XDocument document)
+        {
+            return document.Descendants().Where(n => !n.Elements().Any());
+        } 
+    }
+}
---
+++ XmlParser/XmlParser/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = XDocument.Load("Config.xml");   
+            var output = new XDocument(input.Declaration);
+            var outputRoot = new XElement(input.Root);
+            outputRoot.RemoveNodes();
+            output.Add(outputRoot);
+            foreach (var node in input.Descendants().Where(n => !n.Elements().Any()))
+            {
+                var newNode = new XElement(node.Name);
+                var p = node.Parent;
+                int level = 0;
+                while (p != input.Root)
+                {
+                    string attrName;
+                    if (level == 0)
+                    {
+                        attrName = "parent";
+                    }
+                    else
+                    {
+                        attrName = string.Join("", Enumerable.Repeat("grand",level));
+                    }
+                    newNode.SetAttributeValue(attrName, p.Name);
+                    level++;
+                    p = p.Parent;
+                }
+                newNode.Add(node.Attributes());
+                outputRoot.Add(newNode);
+            }
+            Console.WriteLine(output);
+            output.Save("output.xml");
+            Console.Read();
+        }
+    }
+}
